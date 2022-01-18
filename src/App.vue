14311dc278675efada3dff5e62b7b1b0851eb46a
Vicu <vicugaideic@gmail.com>
2022-01-18 18:58:59 +0300

    input transfer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+
+  data: () => ({
+    show: false,
+    kilometers: false,
+    model: {
+      cm: '1000000',
+      meter: '',
+    }
+  }),
+
+  computed: {
+    meters() {
+      return this.model.cm / 100;
+    },
+    km() {
+      return this.model.cm / 100000;
+    },
+  },
+
+  watch: {
+    model: {
+      immediate: true,
+      deep: true,
+      handler() {
+        if (this.model.cm >= 10000) {
+          this.kilometers = true;
+        } else {
+          this.kilometers = false;
+        }
+      }
+    }
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.show = true;
+      this.$nextTick(() => {
+        this.$refs.input.focus();
+      })
+    },1000);
   },
 };
 </script>
 
-<style>
+
+<template>
+  <div id="app">
+    <input ref="input" v-model="model.cm" v-if="show" type="text" placeholder="cm">
+    <input :value="meters" v-if="show" type="text" placeholder="m">
+    <input :value="km" v-if="kilometers && show" type="text" placeholder="km">
+  </div>
+</template>
+
+
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
